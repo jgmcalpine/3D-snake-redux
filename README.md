7be70de6965ea907fb9df0d7aac4e40517ff1ed9
Jon McAlpine <j.g.mcalpine@gmail.com>
2017-09-11 16:48:34 -0700

    removing some files

--- README.md
+++
@@ -1,2 +0,0 @@
-# 3D-snake-redux
-A 3D snake game in vanilla JS and React/Redux
--- src/index.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import {render} from 'react-dom';
-import {createStore} from 'redux';
-import {Provider} from 'react-redux';
